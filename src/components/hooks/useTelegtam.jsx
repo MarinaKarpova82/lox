d8ec60001110b526136f29f7ffbb66fda08a324d
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-27 13:40:11 +0300

    hh

--- src/components/hooks/useTelegtam.jsx
+++ src/components/hooks/useTelegtam.jsx
@@ -1,4 +1,3 @@
-/* 
 const tg = window.Telegram.WebApp;   
 
 export function useTelegram() 
@@ -20,4 +19,4 @@ tg,
 user: tg.initDataUnsefe?.user,
 queryId: tg.initDataUnsefe?.query_id,
 }
-} */
+} 
