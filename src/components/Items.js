40be86c6e9504598348ab06eb22d62d564fd2326
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-27 11:37:50 +0300

    hh

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react'
 import Item from './Item'
 
-
 export class Items extends Component {
-  render() {
-    return (
-      <main>
-        {this.props.products.map(el => (
-          el.img ? <Item onShowItem={this.props.onShowItem} key={el.id} item={el} onAdd={this.props.onAdd}/> : null
-        ))}
-      </main> 
-    );
-  }
-}
-
-export default Items;
-/* export class Items extends Component {
   render() {
     return (
       <main>
@@ -26,6 +12,5 @@ export default Items;
     )
   }
 }
-
-export default Items */
+export default Items 
 
