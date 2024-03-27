9a9e66b2f378f2eb8eccff7acf1f90bc91468af0
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-27 11:08:28 +0300

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/* import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -32,4 +32,122 @@ function App() {
   )
 }
 
-export default App
+export default App */
+
+import './index.css';
+import './App.css'; 
+import React, { Component } from 'react';
+import Header from './components/Header/Header';
+import Items from './components/Items';
+import ShowFullItem from './components/ShowFullItem';
+
+window.addEventListener('load', async () => {
+  try {
+    if ('serviceWorker' in navigator) {
+      await navigator.serviceWorker.register('/sw.js')
+      console.log('Service Worker registered successfully')
+    } else {
+      console.log('Service Worker not supported')
+    }
+  } catch (error) {
+    console.error('Error registering Service Worker:', error)
+  }
+
+  await loadPosts()
+})
+
+class App extends React.Component {
+ 
+    constructor(props) 
+    {
+      super(props)
+      this.state = {
+        orders: [],
+        products: [
+          {id: '1', img: 'cat (1).jpg', title: 'Коть раз', price: 200, description: 'летучий', descriptionss: 'маленький крылатый котёнок рыже-белого окраса. сидит сидя.'},
+          {id: '2', img: 'cat (2).jpg', title: 'Коть два', price: 230, description: 'летучий', descriptionss: 'горелый летучий кошк. питается исключительно мухами с красными глазами, не приемлет гусениц и жёлтые обои, будьте внимательны перед покупкой'},
+          {id: '3', img: 'cat (3).jpg', title: 'Коть три', price: 999, description: 'летучий', descriptionss: 'летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий'},
+          {id: '4', img: 'cat (4).jpg', title: 'Коть четь', price: 5000, description: 'летучий', descriptionss: 'летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий'},
+          {id: '5', img: 'cat (5).jpg', title: 'Коть пять', price: 750, description: 'летучий', descriptionss: 'летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий'},
+          {id: '6', img: 'cat (6).jpg', title: 'Коть шесь', price: 5, description: 'летучий', descriptionss: 'Кот Бибуп. Бибуп спит, не тревожьте его...'},
+          {id: '7', img: 'cat (7).jpg', title: 'Коть семь', price: 3000, description: 'летучий', descriptionss: 'Блеск данной модели столь велик, что в условиях эксплуатации прямо прописано смотреть нанего исключительно в сварочной маске. Внимательно ознакомьтесь с инструкцией перед покупкой и не дарите ему комплименты, иначе он может засиять ярче.'},
+          {id: '8', img: 'cat (8).jpg', title: 'Коть вось', price: 5000, description: 'летучий', descriptionss: 'летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий'},
+          {id: '9', img: 'cat (9).jpg', title: 'Коть девь', price: 5000, description: 'летучий', descriptionss: 'летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий летучий'},
+        ],
+        showFullItem: false,
+        fullItem: {},
+        totalPrice: 0,
+       imagesLoaded: false ////
+      }
+      this.addToOrder = this.addToOrder.bind(this)
+      this.deleteOrder = this.deleteOrder.bind(this)
+      this.onShowItem = this.onShowItem.bind(this)
+      this.onCloseDescription = this.onCloseDescription.bind(this);
+    }
+
+
+render() {
+  return (
+    <div className="App">
+      <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+      {this.state.imagesLoaded ? (
+        <Items onShowItem={this.onShowItem} products={this.state.products} onAdd={this.addToOrder} />
+      ) : (
+        <div>Loading...</div>
+      )}
+      {this.state.showFullItem && <ShowFullItem item={this.state.fullItem} onCloseDescription={this.onCloseDescription} />}
+    </div>
+  );
+}
+
+/*   
+   render(){
+ return (
+        <div className="App">
+            <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+            <Items onShowItem={this.onShowItem} products={this.state.products} onAdd={this.addToOrder}/>
+            {this.state.showFullItem && <ShowFullItem item={this.state.fullItem} onCloseDescription={this.onCloseDescription} />}
+        </div>
+  ) 
+  }
+
+  */
+  componentDidMount1() {
+    const savedOrders = localStorage.getItem('orders');
+    if (savedOrders) {
+        this.setState({ orders: JSON.parse(savedOrders) });
+    }
+}
+
+  onShowItem(item) {
+    this.setState({fullItem: item})
+    this.setState({showFullItem: !this.state.showFullItem})
+  }
+
+  onCloseDescription() {
+    this.setState({ showFullItem: false });
+}
+
+  deleteOrder(id) {
+
+    this.setState({orders: this.state.orders.filter(el => el.id !== id)})
+
+  }
+
+  addToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach(el => {
+      if(el.id === item.id)
+      isInArray = true
+    })
+    if(!isInArray) {
+        const updatedOrders = [...this.state.orders, item];
+        this.setState({ orders: updatedOrders });
+        localStorage.setItem('orders', JSON.stringify(updatedOrders));
+    }
+}
+
+}
+ 
+export default App; 
+
